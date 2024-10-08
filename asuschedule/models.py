a912ea3a0e7ea7f4fdae614228931f0eb0c2d2c3
bzorn <v.klimenko.2137@gmail.com>
2024-10-08 17:58:10 +0300

    updated user and group models

--- asuschedule/models.py
+++ asuschedule/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -12,14 +12,21 @@ class Group(Base):
     faculty = Column(String, nullable=False)
     speciality = Column(String, nullable=False)
 
+    def get_name(self):
+        return f"{self.course}_{self.faculty}_{self.speciality}"
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, nullable=True)
     name = Column(String, nullable=False)
     subgroup = Column(Integer, nullable=False)  # Подгруппа (1 или 2)
     group_id = Column(Integer, ForeignKey("groups.id"))
+    is_teacher = Column(Boolean, default=False, nullable=True)
+    is_admin = Column(Boolean, default=False, nullable=True)
+    daily_notify = Column(Boolean, default=False, nullable=True)
 
     group = relationship("Group", back_populates="users")
 
